cd8fca95334bda54d418e53cafbb7b3ab614bdb9
Paul Smailis <phsmailis@gmail.com>
2015-11-10 14:36:27 -0500

    ajax start

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function ready(){
+  console.log("document");
+  $('#new_task').on('ajax:success', function(evt, data) {
+    // window.location.reload();
+    $('body').append(data);
+    $('#task_name').val("");
+  }).on("ajax:error", function() { alert("Oops!"); });
+}
+
+$(document).on('ready page:load', ready);
